89a52096f683e803a26af1c850b9aa2c6d87ca86
carlosresendeP <carlosresende.paula@gmail.com>
2025-07-01 21:06:35 -0300

    ajusta configuração da API_URL e adiciona script de builds no package.json

--- interface-Devreceita/src/services/api.js
+++ interface-Devreceita/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
+const API_URL = import.meta.env.VITE_API_URL ;
 
 export const api = async (ask) => {
     try {
